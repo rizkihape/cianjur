2165ca3015f809ad844194c5371d5e87a725bace
rizkihape <haperizki@gmail.com>
2016-05-21 11:55:32 +0700

    index

---
+++ application/controllers/Index.php
@@ -0,0 +1,18 @@
+<?php 
+	/**
+	* 
+	*/
+	class Index extends CI_Controller
+	{
+		
+		function __construct()
+		{
+			parent::__construct();
+		}
+
+		function index()
+		{
+			$this->load->view('Index_view');
+		}
+	}
+ ?>
